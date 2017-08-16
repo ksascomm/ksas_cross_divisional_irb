2c45c232a852758bdd49c329a7ce414c2861aaa1
ksascomm <ksasweb@jhu.edu>
2017-08-16 09:32:34 -0400

    update search bar to use SearchWP

--- front-page.php
+++ front-page.php
@@ -19,17 +19,11 @@
 			    </section>
 
 			    <?php $theme_option = flagship_sub_get_global_options();   //News Query		
-					$news_query_cond = $theme_option['flagship_sub_news_query_cond'];
 					$news_quantity = $theme_option['flagship_sub_news_quantity']; 
-						if ($news_query_cond === 1) {
-							$news_query = new WP_Query(array(
-								'post_type' => 'post',
-								'posts_per_page' => $news_quantity)); 
-						} else {
-							$news_query = new WP_Query(array(
-								'post_type' => 'post',
-								'posts_per_page' => $news_quantity)); 
-						}
+		
+					$news_query = new WP_Query(array(
+						'post_type' => 'post',
+						'posts_per_page' => $news_quantity)); 
 				if ( $news_query->have_posts() ) : ?>
 
 				<div class="news-feed">
