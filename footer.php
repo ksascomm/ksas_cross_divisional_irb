05f84aa2ecbf5f80eb94be899f3a9535c1b9448e
ksascomm <ksasweb@jhu.edu>
2018-03-12 15:55:28 -0400

    jhu shield resized

--- footer.php
+++ footer.php
@@ -5,16 +5,16 @@
 		    						<?php joints_footer_links(); ?>
 		    					</nav>
 		    				</div>
+					  		<div class="row">		   
+						  		<div class="small-12 small-centered medium-4 columns">
+					  				<a href="http://www.jhu.edu" target="_blank"><img class="jhushield" src="<?php echo get_template_directory_uri() ?>/assets/images/jhu-horizontal.png" alt="Johns Hopkins University"/></a>
+					  			</div>	
+					  		</div>		    				
 		    				<div class="row" id="copyright" role="content-info">
 								<div class="small-12 columns">
 									<?php $theme_option = flagship_sub_get_global_options()?>
   									<p>&copy; <?php print date('Y'); ?> Johns Hopkins University, <?php echo $theme_option['flagship_sub_copyright'];?></p>
 					  			</div>
-					  		</div>
-					  		<div class="row">		   
-						  		<div class="small-12 small-centered medium-4 columns">
-					  				<a href="http://www.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/university.jpg" alt="Johns Hopkins University"/></a>
-					  			</div>	
 					  		</div>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
